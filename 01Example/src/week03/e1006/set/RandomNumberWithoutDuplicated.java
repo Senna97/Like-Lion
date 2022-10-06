07d992c9be8a741d6a26f69b0f0288f54a894cc9
Seohyeon <wkdtjgus0319@skuniv.ac.kr>
2022-10-06 10:20:04 +0900

    random number 50개를 생성하고 중복이 되지 않은 숫자만 남기는 것

--- 01Example/src/week03/e1006/set/RandomNumberWithoutDuplicated.java
+++ 01Example/src/week03/e1006/set/RandomNumberWithoutDuplicated.java
@@ -3,18 +3,18 @@ package week03.e1006.set;
 import week03.e1005.dependencyinjection.RandomNumberGenerator;
 
 import java.util.HashSet;
-import java.util.Set;
 
 public class RandomNumberWithoutDuplicated {
     public static void main(String[] args) {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
-        Set<Integer> randomNumber = new HashSet<>();
+        HashSet<Integer> randomNumbers = new HashSet<>();
 
         for (int i = 0; i < 50; i++) {
-            randomNumber.add(randomNumberGenerator.Generate(10));
+            randomNumbers.add(randomNumberGenerator.Generate(100));
         }
 
-        System.out.println("random number 50개를 생성하고 중복이 되지 않은 숫자만 남기는 것: " + randomNumber);
+        System.out.println("random number 50개를 생성하고 중복이 되지 않은 숫자만 남기는 것: " + randomNumbers);
+        System.out.println("갯수: " + randomNumbers.size());
     }
 }
