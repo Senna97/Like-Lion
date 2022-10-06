1658f73c58f68b18412191f3a9d048b45e024517
Seohyeon <wkdtjgus0319@skuniv.ac.kr>
2022-10-06 15:12:11 +0900

    파일 목록

---
+++ 01Example/src/week03/e1006/file/FileList.java
@@ -0,0 +1,11 @@
+package week03.e1006.file;
+
+import java.io.File;
+
+public class FileList {
+    public static void main(String[] args) {
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        System.out.println(files[0]);
+    }
+}
