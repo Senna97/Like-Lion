6ee4c546922b631fa6311b281341d22e97a8e89c
Seohyeon <wkdtjgus0319@skuniv.ac.kr>
2022-10-17 09:19:36 +0900

    RightTriangle Commit

--- 01Example/src/week05/e1017/algorithm/RightTriangle.java
+++ 01Example/src/week05/e1017/algorithm/RightTriangle.java
@@ -3,15 +3,21 @@ package week05.e1017.algorithm;
 import java.util.Scanner;
 
 public class RightTriangle {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
 
+    public void getRightTriangle(int num) {
         for (int i = 0; i < num; i++) {
             for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        RightTriangle rightTriangle = new RightTriangle();
+        rightTriangle.getRightTriangle(num);
     }
 }
