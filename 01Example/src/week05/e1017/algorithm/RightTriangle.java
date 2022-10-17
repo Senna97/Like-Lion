f9bd3d0378f05621d5f5df451efac2f9c292ee7b
Seohyeon <wkdtjgus0319@skuniv.ac.kr>
2022-10-17 09:35:32 +0900

    Challenge Commit

--- 01Example/src/week05/e1017/algorithm/RightTriangle.java
+++ 01Example/src/week05/e1017/algorithm/RightTriangle.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 
 public class RightTriangle {
 
-    public void getRightTriangle(int num, char ch) {
+    private String letter = "*";
+
+    public RightTriangle() {
+    }
+
+    public RightTriangle(String letter) {
+        this.letter = letter;
+    }
+
+    public void getRightTriangle(int num) {
         for (int i = 0; i < num; i++) {
             for (int j = 0; j < i + 1; j++) {
-                System.out.print(ch);
+                System.out.print(this.letter);
             }
             System.out.println();
         }
@@ -15,11 +24,10 @@ public class RightTriangle {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("몇 줄의 어떤 피라미드를 찍고 싶으신가요? (정수와 문자 입력) >> ");
+        System.out.print("몇 줄의 피라미드를 찍고 싶으신가요? >> ");
         int num = sc.nextInt();
-        char ch = sc.next().charAt(0);
 
-        RightTriangle rightTriangle = new RightTriangle();
-        rightTriangle.getRightTriangle(num, ch);
+        RightTriangle rightTriangle = new RightTriangle("%");
+        rightTriangle.getRightTriangle(num);
     }
 }
