074fd1a991f6436c66488896fd5abe38c7a0c36b
Seohyeon <wkdtjgus0319@skuniv.ac.kr>
2022-10-17 10:25:08 +0900

    Pyramid Commit

--- 01Example/src/week05/e1017/algorithm/Pyramid.java
+++ 01Example/src/week05/e1017/algorithm/Pyramid.java
@@ -9,13 +9,15 @@ public class Pyramid {
         int num = sc.nextInt();
 
         for (int i = 0; i < num; i++) {
+
             for (int j = 1; j < num - i; j++) {
                 System.out.print(" ");
             }
 
-            for (int j = 0; j < i + 1; j++) {
+            for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
             }
+
             System.out.println();
         }
     }
