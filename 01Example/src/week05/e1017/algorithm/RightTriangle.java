ec96fc60b2ac9eed39c8ea541eecc6ce5967de35
Seohyeon <wkdtjgus0319@skuniv.ac.kr>
2022-10-17 09:09:47 +0900

    RightTriangle Commit

---
+++ 01Example/src/week05/e1017/algorithm/RightTriangle.java
@@ -0,0 +1,11 @@
+package week05.e1017.algorithm;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+    }
+}
