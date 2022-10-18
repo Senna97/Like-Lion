3af9ceb2bf8a3c8a6a1913d95b4590ec3e087fde
Seohyeon <wkdtjgus0319@skuniv.ac.kr>
2022-10-18 09:12:07 +0900

    Square Commit

---
+++ 01Example/src/week05/a1018/algorithm/Square.java
@@ -0,0 +1,33 @@
+package week05.a1018.algorithm;
+
+import java.util.Scanner;
+
+public class Square {
+    String letter = "*";
+
+    public Square() {
+    }
+
+    public Square(String letter) {
+        this.letter = letter;
+    }
+
+    public void printSquare(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(letter);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("n * n 정사각형을 그립니다. >> ");
+        int n = sc.nextInt();
+
+        Square square = new Square("@");
+        square.printSquare(n);
+    }
+}
