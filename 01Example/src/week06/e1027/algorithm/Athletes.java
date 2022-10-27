ea6b02ad8606533d14a60680904cf58fe715e65f
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-10-27 10:47:02 +0900

    완주하지 못한 선수 커밋3 getOrDefault 사용

--- 01Example/src/week06/e1027/algorithm/Athletes.java
+++ 01Example/src/week06/e1027/algorithm/Athletes.java
@@ -8,17 +8,12 @@ public class Athletes {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> memo = new HashMap<>();
 
-        for (int i = 0; i < participant.length; i++) {
-            String key = participant[i];
-            if (!memo.containsKey(key)) {
-                memo.put(key, 0);
-            }
-            memo.put(key, memo.get(key) + 1);
+        for (String member : participant) {
+            memo.put(member, memo.getOrDefault(member, 0) + 1);
         }
 
-        for (int i = 0; i < completion.length; i++) {
-            String key = completion[i];
-            memo.put(key, memo.get(key) - 1);
+        for (String member : completion) {
+            memo.put(member, memo.getOrDefault(member, 0) - 1);
         }
 
         for (String key : memo.keySet()) {
