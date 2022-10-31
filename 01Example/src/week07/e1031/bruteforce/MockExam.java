cd3dffea22784e3a40c2d085483878aab4aa751c
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-10-31 10:34:57 +0900

    나머지를 이용하는 방법

--- 01Example/src/week07/e1031/bruteforce/MockExam.java
+++ 01Example/src/week07/e1031/bruteforce/MockExam.java
@@ -4,22 +4,24 @@ import java.util.Arrays;
 
 public class MockExam {
 
-    public int solution(int[] answers) { // 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return
-//        int[] answer = {};
+    public int[] solution(int[] answers) { // 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return
+        int[] answer = {0, 0, 0};
 
-        int[] firstStudentAnswer = new int[]{1, 2, 3, 4, 5};
-        int[] secondStudentAnswer = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
-        int[] thirdStudentAnswer = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] firstStudentAnswer = {1, 2, 3, 4, 5};
+        int[] secondStudentAnswer = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] thirdStudentAnswer = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        int firstStudentAnswerCount = 0;
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] % firstStudentAnswer[i] == 0) {
-                firstStudentAnswerCount++;
+            if (answers[i] == firstStudentAnswer[i % firstStudentAnswer.length]) {
+                answer[0] += 1;
+            } else if (answers[i] == secondStudentAnswer[i % secondStudentAnswer.length]) {
+                answer[1] += 1;
+            } else if (answers[i] == thirdStudentAnswer[i % thirdStudentAnswer.length]) {
+                answer[2] += 1;
             }
         }
-        return firstStudentAnswerCount;
 
-//        return answer;
+        return answer;
     }
 
     public static void main(String[] args) {
@@ -27,10 +29,10 @@ public class MockExam {
         int[] ex1 = new int[]{1, 2, 3, 4, 5};
         int[] ex2 = new int[]{1, 3, 2, 4, 2};
 
-        int result1 = mockExam.solution(ex1);
-        System.out.println(result1);
+        int[] result1 = mockExam.solution(ex1);
+        System.out.println(Arrays.toString(result1));
 
-        int result2 = mockExam.solution(ex2);
-        System.out.println(result2);
+        int[] result2 = mockExam.solution(ex2);
+        System.out.println(Arrays.toString(result2));
     }
 }
