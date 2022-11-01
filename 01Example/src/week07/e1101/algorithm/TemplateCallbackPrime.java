94c112ef93d9e69c4022224db2d79fbaa13ad4a6
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-01 10:30:44 +0900

    2단계 StatementStrategy 적용

--- 01Example/src/week07/e1101/algorithm/TemplateCallbackPrime.java
+++ 01Example/src/week07/e1101/algorithm/TemplateCallbackPrime.java
@@ -1,14 +1,14 @@
 package week07.e1101.algorithm;
 
+interface StatementStrategy {
+    boolean compare(int a, int b);
+}
+
 public class TemplateCallbackPrime {
 
-    boolean someOperation(int a, int b) {
-        return a < b;
-    }
+    boolean isPrime(int n, StatementStrategy stmt) {
 
-    boolean isPrime(int n) {
-
-        for (int i = 2; someOperation(i, n); i++) {
+        for (int i = 2; stmt.compare(i, n); i++) {
             if (n % i == 0) {
                 return false;
             }
