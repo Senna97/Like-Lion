f7771effa9f5eef6e85e2ef670772d137726a4aa
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-01 09:39:22 +0900

    13, 17, 19, 23이 소수인지 판별하는 알고리즘(나머지 연산)

--- 01Example/src/week07/e1101/algorithm/PrimeNumber.java
+++ 01Example/src/week07/e1101/algorithm/PrimeNumber.java
@@ -8,17 +8,13 @@ public class PrimeNumber {
     }
 
     boolean isPrime(int n) {
-        boolean answer = false;
         int flag = 0;
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
                 flag++;
             }
         }
-        if (flag == 0) {
-            answer = true;
-        }
-        return answer;
+        return flag == 0;
     }
 
     public static void main(String[] args) {
