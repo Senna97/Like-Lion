e8f4fe64baa72322d851e02f422d9a808daf92fb
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-01 10:50:27 +0900

    3단계: 익명 내부 클래스 & 4단계: 람다식 표현

--- 01Example/src/week07/e1101/algorithm/TemplateCallbackPrime.java
+++ 01Example/src/week07/e1101/algorithm/TemplateCallbackPrime.java
@@ -18,7 +18,16 @@ public class TemplateCallbackPrime {
 
     public static void main(String[] args) {
         TemplateCallbackPrime tcp = new TemplateCallbackPrime();
-        boolean result = tcp.isPrime(17);
+        boolean result = tcp.isPrime(17, new StatementStrategy() {
+            @Override
+            public boolean compare(int a, int b) {
+                return a * a <= b;
+            }
+        });
         System.out.println(result);
+
+        System.out.println(tcp.isPrime(13, (a, b) -> a < b));
+        System.out.println(tcp.isPrime(17, (a, b) -> a < b / 2));
+        System.out.println(tcp.isPrime(19, (a, b) -> a * a <= b));
     }
 }
