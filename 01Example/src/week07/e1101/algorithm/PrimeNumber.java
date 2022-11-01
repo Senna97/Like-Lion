a577fe4d96ddc0fa2c8785736dc4685483471760
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-01 09:27:33 +0900

    13, 17, 19, 23이 소수인지 판별하는 알고리즘(나머지 연산)

--- 01Example/src/week07/e1101/algorithm/PrimeNumber.java
+++ 01Example/src/week07/e1101/algorithm/PrimeNumber.java
@@ -4,13 +4,15 @@ public class PrimeNumber {
 
     public int solution(int n) {
         int answer = 0;
+        return answer;
+    }
+
+    boolean isPrime(int n) {
+        boolean answer = false;
         for (int i = 2; i <= n; i++) {
             if (n % i != 0) {
-                answer++;
+                answer = true;
             }
-        }
-        if (answer != 0) {
-            System.out.println("소수입니다.");
         }
         return answer;
     }
@@ -21,10 +23,13 @@ public class PrimeNumber {
 //        System.out.println(solution1); // 2, 3, 5, 7 => 4
 //        int solution2 = primeNumber.solution(5); // 2, 3, 5 => 3
 //        System.out.println(solution2);
-        primeNumber.solution(13);
-        primeNumber.solution(17);
-        primeNumber.solution(19);
-        primeNumber.solution(23);
-
+        boolean prime1 = primeNumber.isPrime(13);
+        System.out.println(prime1);
+        boolean prime2 = primeNumber.isPrime(17);
+        System.out.println(prime2);
+        boolean prime3 = primeNumber.isPrime(19);
+        System.out.println(prime3);
+        boolean prime4 = primeNumber.isPrime(23);
+        System.out.println(prime4);
     }
 }
