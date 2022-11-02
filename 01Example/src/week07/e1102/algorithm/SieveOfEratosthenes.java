da36e71ee45b9fdc5c8e39e13cd3ed0a6536527b
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-02 10:03:48 +0900

    에라토스테네스의 체 코드 구현 완성, but 시간초과

--- 01Example/src/week07/e1102/algorithm/SieveOfEratosthenes.java
+++ 01Example/src/week07/e1102/algorithm/SieveOfEratosthenes.java
@@ -2,17 +2,17 @@ package week07.e1102.algorithm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SieveOfEratosthenes {
 
-    void findPrime() {
-        int capacity = 49;
-        List<Integer> list = new ArrayList<>(capacity);
-        for (int i = 0; i < capacity; i++) {
+    void findPrime(int n) {
+        List<Integer> list = new ArrayList<>(n - 1);
+        for (int i = 0; i < n - 1; i++) {
             list.add(i + 2);
         }
 
-        for (int i = 2; i <= Math.sqrt(capacity); i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             for (int j = 0; j < list.size(); j++) {
                 if (list.get(j) % i == 0 && list.get(j) != i) {
                     list.remove(j);
@@ -25,7 +25,10 @@ public class SieveOfEratosthenes {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환합니다. 숫자 n 입력 >> ");
+        int n = sc.nextInt();
         SieveOfEratosthenes sieveOfEratosthenes = new SieveOfEratosthenes();
-        sieveOfEratosthenes.findPrime();
+        sieveOfEratosthenes.findPrime(n);
     }
 }
