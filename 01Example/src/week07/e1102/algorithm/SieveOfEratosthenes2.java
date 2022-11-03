6e1eea4178f278b414d430c14e1c19452a75911e
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-03 10:01:55 +0900

    문제 풀기 전 초기 세팅

--- 01Example/src/week07/e1102/algorithm/SieveOfEratosthenes2.java
+++ 01Example/src/week07/e1102/algorithm/SieveOfEratosthenes2.java
@@ -6,19 +6,19 @@ public class SieveOfEratosthenes2 {
 
     public int solution(int n) {
         int[] nums = new int[n - 1];
-        boolean[] checks = new boolean[n - 1];
+        boolean[] checks = new boolean[nums.length];
 
-        int num = 2;
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = num;
-            num++;
+            nums[i] = i + 2;
             checks[i] = true;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0 && nums[i] != 2) {
-                checks[i] = false;
-            }
+        for (int i = 2; i < n; i += 2) { // 시작이 4부터
+            checks[i] = false;
+        }
+
+        for (int i = 4; i < n; i += 3) { // 시작이 6부터
+            checks[i] = false;
         }
 
         int count = 0;
@@ -33,7 +33,7 @@ public class SieveOfEratosthenes2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        System.out.print("1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환합니다. 숫자 n 입력 >> ");
+        System.out.print("1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환합니다. 숫자 n 입력 >> ");
         int n = sc.nextInt();
 
         SieveOfEratosthenes2 sieveOfEratosthenes2 = new SieveOfEratosthenes2();
