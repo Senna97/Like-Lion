27874150729d04f59fe8c2142b59935a6900cfb3
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-07 09:44:51 +0900

    나누어 떨어지는 숫자 배열 완료

---
+++ 01Example/src/week08/algorithm/DivisibleArray.java
@@ -0,0 +1,32 @@
+package week08.algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DivisibleArray {
+
+    public List<Integer> solution(int[] arr, int divisor) {
+        List<Integer> answer = new ArrayList<>();
+        for (int element : arr) {
+            if (element % divisor == 0) {
+                answer.add(element);
+            }
+        }
+
+        if (answer.size() == 0) {
+            answer.add(-1);
+        }
+
+        Collections.sort(answer);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 6};
+        int divisor = 10;
+        DivisibleArray divisibleArray = new DivisibleArray();
+        List<Integer> solution = divisibleArray.solution(arr, divisor);
+        System.out.println(solution);
+    }
+}
