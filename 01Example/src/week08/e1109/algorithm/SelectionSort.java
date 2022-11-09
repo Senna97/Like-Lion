f52c8f8de136035d524c298e5623c63a830d84c4
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-09 10:55:52 +0900

    선택정렬 오름차순&내림차순 기능 구현: BiFunction 사용

--- 01Example/src/week08/e1109/algorithm/SelectionSort.java
+++ 01Example/src/week08/e1109/algorithm/SelectionSort.java
@@ -1,14 +1,15 @@
 package week08.e1109.algorithm;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public class SelectionSort {
 
-    public void selectionSort(int[] arr, SortStrategy stst) {
+    public void selectionSort(int[] arr, BiFunction<Integer, Integer, Boolean> bFn) {
         for (int i = 0; i < arr.length - 1; i++) {
             int swapIdx = i;
             for (int j = i; j < arr.length; j++) {
-                if (stst.apply(arr[swapIdx], arr[j])) {
+                if (bFn.apply(arr[j], arr[swapIdx])) {
                     swapIdx = j;
                 }
             }
@@ -24,7 +25,7 @@ public class SelectionSort {
         SelectionSort selectionSort = new SelectionSort();
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
 
-        selectionSort.selectionSort(arr, (a, b) -> a > b);// 오름차순 정렬
-        selectionSort.selectionSort(arr, (a, b) -> a < b);// 내림차순 정렬
+        selectionSort.selectionSort(arr, (a, b) -> a < b);// 오름차순 정렬
+        selectionSort.selectionSort(arr, (a, b) -> a > b);// 내림차순 정렬
     }
 }
