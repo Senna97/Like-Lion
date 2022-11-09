cba795d864c4c9708ff4f8c4743417958ddeca5b
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-09 10:34:40 +0900

    선택정렬 내림차순 정렬 기능: Interface 선언 후 Template Callback 패턴 적용

--- 01Example/src/week08/e1109/algorithm/SelectionSort.java
+++ 01Example/src/week08/e1109/algorithm/SelectionSort.java
@@ -3,20 +3,27 @@ package week08.e1109.algorithm;
 import java.util.Arrays;
 
 public class SelectionSort {
-    public static void main(String[] args) {
-        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
 
+    public void selectionSort(int[] arr, SortStrategy stst) {
         for (int i = 0; i < arr.length - 1; i++) {
-            int minIdx = i;
+            int swapIdx = i;
             for (int j = i; j < arr.length; j++) {
-                if (arr[j] < arr[minIdx]) {
-                    minIdx = j;
+                if (stst.apply(arr[swapIdx], arr[j])) {
+                    swapIdx = j;
                 }
             }
             int temp = arr[i];
-            arr[i] = arr[minIdx];
-            arr[minIdx] = temp;
+            arr[i] = arr[swapIdx];
+            arr[swapIdx] = temp;
         }
         System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        SelectionSort selectionSort = new SelectionSort();
+        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+
+        selectionSort.selectionSort(arr, (a, b) -> a > b);// 오름차순 정렬
+        selectionSort.selectionSort(arr, (a, b) -> a < b);// 내림차순 정렬
     }
 }
