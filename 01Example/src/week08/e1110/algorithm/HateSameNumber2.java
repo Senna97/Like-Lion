c5030e1b657450cafbbd8351833b7f6b76b7e796
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-10 10:10:05 +0900

    프로그래머스: 같은 숫자는 싫어 - 완료 (Stack 사용 O)

---
+++ 01Example/src/week08/e1110/algorithm/HateSameNumber2.java
@@ -0,0 +1,28 @@
+package week08.e1110.algorithm;
+
+import java.util.List;
+import java.util.Stack;
+
+public class HateSameNumber2 {
+
+    public List<Integer> solution(int[] arr) {
+
+        Stack<Integer> stack = new Stack<>();
+        for (int element : arr) {
+            if (stack.isEmpty() || stack.peek() != element) {
+                stack.add(element);
+            }
+        }
+
+        return stack;
+    }
+
+    public static void main(String[] args) {
+        HateSameNumber2 hateSameNumber2 = new HateSameNumber2();
+        int[] arr1 = {1, 1, 3, 3, 0, 1, 1};
+        int[] arr2 = {4, 4, 4, 3, 3};
+
+        System.out.println(hateSameNumber2.solution(arr1)); // [1,3,0,1]
+        System.out.println(hateSameNumber2.solution(arr2)); // [4, 3]
+    }
+}
