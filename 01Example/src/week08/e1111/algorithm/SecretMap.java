be9e231e15873d84523078532fc3e4e562deaa91
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-11 10:08:25 +0900

    프로그래머스: [1차] 비밀지도 - 개수에 맞는 이진수 배열(String.format 사용)

--- 01Example/src/week08/e1111/algorithm/SecretMap.java
+++ 01Example/src/week08/e1111/algorithm/SecretMap.java
@@ -7,17 +7,16 @@ public class SecretMap {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
 
-        String[][] arr = new String[n][n]; // 2차원 배열 생성
-        String[] str1 = new String[n];
-        String[] str2 = new String[n];
+        String[] sArr1 = new String[n];
+        String[] sArr2 = new String[n];
 
         for (int i = 0; i < n; i++) { // 10진수 -> 2진수 변환 & 자릿수 맞추기
-            str1[i] = String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr1[i])));
-            str2[i] = String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr2[i])));
+            sArr1[i] = String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr1[i])));
+            sArr2[i] = String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr2[i])));
         }
 
-        System.out.println(Arrays.toString(str1));
-        System.out.println(Arrays.toString(str2));
+        System.out.println(Arrays.toString(sArr1));
+        System.out.println(Arrays.toString(sArr2));
 
         return answer;
     }
