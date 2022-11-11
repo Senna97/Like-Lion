b778f134b6a0e8e6c330f59efaed32af72b45a08
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-11 09:28:20 +0900

    프로그래머스: [1차] 모두 5자리로 만들기

--- 01Example/src/week08/e1111/algorithm/SecretMap.java
+++ 01Example/src/week08/e1111/algorithm/SecretMap.java
@@ -4,14 +4,22 @@ import java.util.Arrays;
 
 public class SecretMap {
 
-    public int[] solution(int n, int[] arr1, int[] arr2) {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
+        String[][] arr = new String[n][n]; // 2차원 배열 생성
+        String[] str1 = new String[n];
+        String[] str2 = new String[n];
 
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = Integer.parseInt(Integer.toBinaryString(arr1[i]));
+
+        for (int i = 0; i < n; i++) { // 10진수 -> 2진수 변환
+            str1[i] = String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr1[i])));
+            str2[i] = String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr2[i])));
         }
 
-        return arr1;
+        System.out.println(Arrays.toString(str1));
+        System.out.println(Arrays.toString(str2));
+
+        return answer;
     }
 
     public static void main(String[] args) {
@@ -20,7 +28,7 @@ public class SecretMap {
         int[] arr2 = {30, 1, 21, 17, 28};
 
         SecretMap secretMap = new SecretMap();
-        int[] solution = secretMap.solution(n, arr1, arr2);
+        String[] solution = secretMap.solution(n, arr1, arr2);
         System.out.println(Arrays.toString(solution)); // ["#####","# # #", "### #", "# ##", "#####"]
     }
 }
