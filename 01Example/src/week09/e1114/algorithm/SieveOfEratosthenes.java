a97ec5fdfb9b8fe50b08d6319f2644146741c36f
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-14 10:56:49 +0900

    에라토스테네스의 체 복습 완성

--- 01Example/src/week09/e1114/algorithm/SieveOfEratosthenes.java
+++ 01Example/src/week09/e1114/algorithm/SieveOfEratosthenes.java
@@ -1,52 +1,35 @@
 package week09.e1114.algorithm;
 
-import java.util.Arrays;
-
 public class SieveOfEratosthenes {
-    public static void main(String[] args) {
-        int n = 50;
+
+    public int solution(int n) {
         int[] nums = new int[n - 1];
 
         for (int i = 0; i < nums.length; i++) {
             nums[i] = i + 2;
         }
 
-        //2 4 6 8 ...
         int idx = 0;
-        for (int i = nums[idx] + 0; i < nums.length; i += 2) {
-            nums[i] = 0;
+        for (int i = 2; i <= Math.sqrt(nums.length); i++) {
+            for (int j = nums[idx] + idx; j < nums.length; j += i) {
+                nums[j] = 0;
+            }
+            idx++;
         }
 
-        //4 7 10 13 ...
-        idx = 1;
-        for (int i = nums[idx] + 1; i < nums.length; i += 3) {
-            nums[i] = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                count++;
+            }
         }
 
-        //6 10 14 18...
-        idx = 2;
-        for (int i = nums[idx] + 2; i < nums.length; i += 4) {
-            nums[i] = 0;
-        }
+        return count;
+    }
 
-        //8 13 18 23...
-        idx = 3;
-        for (int i = nums[idx] + 3; i < nums.length; i += 5) {
-            nums[i] = 0;
-        }
-
-        //10 16 22 28...
-        idx = 4;
-        for (int i = nums[idx] + 4; i < nums.length; i += 6) {
-            nums[i] = 0;
-        }
-
-        //12 19 26 33...
-        idx = 5;
-        for (int i = nums[idx] + 5; i < nums.length; i += 7) {
-            nums[i] = 0;
-        }
-
-        System.out.println(Arrays.toString(nums));
+    public static void main(String[] args) {
+        SieveOfEratosthenes sieveOfEratosthenes = new SieveOfEratosthenes();
+        int solution = sieveOfEratosthenes.solution(50);
+        System.out.println(solution);
     }
 }
