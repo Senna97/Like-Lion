6170313305fbb15f6780725f873a4d44e7811054
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-15 10:48:06 +0900

    퀵 정렬 완료(탈출조건⭐️)

--- 01Example/src/week09/e1115/algorithm/QuickSort.java
+++ 01Example/src/week09/e1115/algorithm/QuickSort.java
@@ -6,20 +6,26 @@ import java.util.List;
 public class QuickSort {
 
     public List<Integer> sort(List<Integer> arr) {
+
+        if (arr.size() <= 1) {
+            return arr;
+        }
+
+        // 1. 기준값 뽑기
+        int pivot = arr.get(arr.size() / 2);
+
+        // 2. 기준값 기준으로 좌, 우로 나누기
         List<Integer> low = new ArrayList<>();
         List<Integer> mid = new ArrayList<>();
         List<Integer> high = new ArrayList<>();
 
-        // 1. 기준값 뽑기
-        int pivot = arr.get(arr.size() / 2);
-        mid.add(pivot);
-
-        // 2. 기준값 기준으로 좌, 우로 나누기
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i) < pivot) {
-                low.add(arr.get(i));
-            } else if (arr.get(i) > pivot) {
-                high.add(arr.get(i));
+        for (Integer element : arr) {
+            if (element < pivot) {
+                low.add(element);
+            } else if (element > pivot) {
+                high.add(element);
+            } else {
+                mid.add(pivot);
             }
         }
 
@@ -37,6 +43,7 @@ public class QuickSort {
 
     public static void main(String[] args) {
         int[] nums = {20, 18, 5, 19, 40, 50, 5, 25};
+
         List<Integer> numsList = new ArrayList<>();
         for (int num : nums) {
             numsList.add(num);
