4a0280cf4466a60541794e8c101fb1faa3e817fe
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-15 10:32:44 +0900

    퀵 정렬 IndexOutOfBoundsException 발생

--- 01Example/src/week09/e1115/algorithm/QuickSort.java
+++ 01Example/src/week09/e1115/algorithm/QuickSort.java
@@ -4,27 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QuickSort {
-    public static void main(String[] args) {
-        int[] nums = {20, 18, 5, 19, 40, 50, 5, 25};
-        List<Integer> left = new ArrayList<>();
-        List<Integer> mid = new ArrayList<>();
-        List<Integer> right = new ArrayList<>();
 
-        // 기준값 뽑기
-        int midIdx = nums.length / 2; // 4
-        int pivot = nums[midIdx]; // 40
+    public List<Integer> sort(List<Integer> arr) {
+        List<Integer> low = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
+        List<Integer> high = new ArrayList<>();
+
+        // 1. 기준값 뽑기
+        int pivot = arr.get(arr.size() / 2);
         mid.add(pivot);
 
-        for (int num : nums) {
-            if (num < pivot) {
-                left.add(num);
-            } else if (num > pivot) {
-                right.add(num);
+        // 2. 기준값 기준으로 좌, 우로 나누기
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) < pivot) {
+                low.add(arr.get(i));
+            } else if (arr.get(i) > pivot) {
+                high.add(arr.get(i));
             }
         }
 
-        System.out.println(left);
-        System.out.println(mid);
-        System.out.println(right);
+        return join(sort(low), mid, sort(high));
+    }
+
+    public List<Integer> join(List<Integer> low, List<Integer> mid, List<Integer> high) {
+        List<Integer> joined = new ArrayList<>();
+        joined.addAll(low);
+        joined.addAll(mid);
+        joined.addAll(high);
+
+        return joined;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {20, 18, 5, 19, 40, 50, 5, 25};
+        List<Integer> numsList = new ArrayList<>();
+        for (int num : nums) {
+            numsList.add(num);
+        }
+
+        QuickSort quickSort = new QuickSort();
+        List<Integer> result = quickSort.sort(numsList);
+        System.out.println(result);
     }
 }
