33668c036bc418067402c03c4bc24c1384268950
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-18 10:00:12 +0900

    프로그래머스 시저 암호 완료!

--- 01Example/src/week09/e1118/algorithm/CaesarCipher.java
+++ 01Example/src/week09/e1118/algorithm/CaesarCipher.java
@@ -7,13 +7,13 @@ public class CaesarCipher {
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
-                if (s.charAt(i) >= 'z') {
+                if (s.charAt(i) + n > 'Z') {
                     answer.append((char) (s.charAt(i) - 26 + n));
                 } else {
                     answer.append((char) (s.charAt(i) + n));
                 }
             } else if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
-                if (s.charAt(i) >= 'z') {
+                if (s.charAt(i) + n > 'z') {
                     answer.append((char) (s.charAt(i) - 26 + n));
                 } else {
                     answer.append((char) (s.charAt(i) + n));
