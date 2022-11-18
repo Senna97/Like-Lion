33a54bda70efbada9f03da069920c85293190bef
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-18 10:11:35 +0900

    프로그래머스 시저 암호 - 메서드로 추출

--- 01Example/src/week09/e1118/algorithm/CaesarCipher.java
+++ 01Example/src/week09/e1118/algorithm/CaesarCipher.java
@@ -7,22 +7,32 @@ public class CaesarCipher {
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
-                if (s.charAt(i) + n > 'Z') {
-                    answer.append((char) (s.charAt(i) - 26 + n));
-                } else {
-                    answer.append((char) (s.charAt(i) + n));
-                }
+                answer.append(upperReplace(s.charAt(i), n));
             } else if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
-                if (s.charAt(i) + n > 'z') {
-                    answer.append((char) (s.charAt(i) - 26 + n));
-                } else {
-                    answer.append((char) (s.charAt(i) + n));
-                }
+                answer.append(lowerReplace(s.charAt(i), n));
             } else if (s.charAt(i) == ' ') {
                 answer.append(' ');
             }
         }
         return answer.toString();
+    }
+
+    public char upperReplace(char ch, int n) {
+        if (ch + n > 'Z') {
+            ch = (char)(ch - 26 + n);
+        } else {
+            ch = (char)(ch + n);
+        }
+        return ch;
+    }
+
+    public char lowerReplace(char ch, int n) {
+        if (ch + n > 'z') {
+            ch = (char)(ch - 26 + n);
+        } else {
+            ch = (char)(ch + n);
+        }
+        return ch;
     }
 
     public static void main(String[] args) {
