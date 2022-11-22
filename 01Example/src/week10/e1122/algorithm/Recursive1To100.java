9e54b2aed5b108086a2c20808ed9b2164f8de35f
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-22 09:27:06 +0900

    1에서 100까지 재귀함수로 출력하기

--- 01Example/src/week10/e1122/algorithm/Recursive1To100.java
+++ 01Example/src/week10/e1122/algorithm/Recursive1To100.java
@@ -1,17 +1,17 @@
 package week10.e1122.algorithm;
 
-public class RecursiveFunctionPractice {
+public class Recursive1To100 {
 
     public void print1To100(int num) {
         if (num > 100) {
             return;
         }
         System.out.println(num);
-        print1To100(num);
+        print1To100(num + 1);
     }
 
     public static void main(String[] args) {
-        RecursiveFunctionPractice recursiveFunctionPractice = new RecursiveFunctionPractice();
-        recursiveFunctionPractice.print1To100(1);
+        Recursive1To100 recursive1To100 = new Recursive1To100();
+        recursive1To100.print1To100(1);
     }
 }
