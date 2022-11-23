3f8e8c29a31f4e18475818d368f451e16574e2e3
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-23 10:47:21 +0900

    n번째 피보나치 수 리턴하기(재귀X)

--- 01Example/src/week10/e1123/algorithm/FibonacciNumbers.java
+++ 01Example/src/week10/e1123/algorithm/FibonacciNumbers.java
@@ -5,17 +5,23 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FibonacciNumbers {
+
+    public int getFibonacciNum(int num) {
+        List<Integer> fibonacciList = new ArrayList<>();
+        fibonacciList.add(1);
+        fibonacciList.add(1);
+        for (int i = 0; i < num - 2; i++) {
+            fibonacciList.add(fibonacciList.get(i) + fibonacciList.get(i + 1));
+        }
+        return fibonacciList.get(num - 1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        List<Integer> fibonacciList = new ArrayList<>();
-        fibonacciList.add(1);
-        fibonacciList.add(1);
-        for (int i = 0; i < n - 2; i++) {
-            fibonacciList.add(fibonacciList.get(i) + fibonacciList.get(i + 1));
-        }
-        System.out.println(fibonacciList);
-        System.out.println(fibonacciList.get(n - 1));
+        FibonacciNumbers fibonacciNumbers = new FibonacciNumbers();
+        int answer = fibonacciNumbers.getFibonacciNum(n);
+        System.out.println(answer);
     }
 }
