5397eb7560be5e36272b25c427d75be79fbafdf8
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-29 10:24:51 +0900

    배열 원소 자릿수 구해서 중복제거

--- 01Example/src/week11/e1129/algorithm/RadixSort.java
+++ 01Example/src/week11/e1129/algorithm/RadixSort.java
@@ -5,6 +5,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RadixSort {
+
+    public int getRemainder(int num, int count) {
+        if (num >= 10) {
+            return getRemainder(num / 10, count += 1);
+        } else {
+            return count;
+        }
+    }
+
+    public int getHighestDigit(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = getRemainder(arr[i], 1);
+        }
+
+        int max = 0;
+        for (int num : arr) {
+            max = Math.max(0, num);
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
         int[] intArr = new int[]{7, 4, 5, 9, 1, 0};
         int[] emptyArr = new int[10];
@@ -12,8 +34,8 @@ public class RadixSort {
 
         Arrays.fill(emptyArr, -1);
 
-        for (int i = 0; i < intArr.length; i++) {
-            emptyArr[intArr[i]] = intArr[i];
+        for (int num : intArr) {
+            emptyArr[num] = num;
         }
 
         for (int num : emptyArr) {
@@ -23,5 +45,10 @@ public class RadixSort {
         }
 
         System.out.println(answer);
+
+        int[] intArr2 = new int[]{71, 4, 125, 12395, 1234, 123450};
+        RadixSort radixSort = new RadixSort();
+        int highestDigit = radixSort.getHighestDigit(intArr2);
+        System.out.println(highestDigit);
     }
 }
