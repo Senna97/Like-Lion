6271786633a1e2e2d20ef606d40323855a7200e4
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-12-08 10:47:57 +0900

    실습: str2.length() * str1.length() 사이즈의 int 배열 만들기

---
+++ 01Example/src/week12/e1208/algorithm/LCS.java
@@ -0,0 +1,12 @@
+package week12.e1208.algorithm;
+
+import java.util.Arrays;
+
+public class LCS {
+    public static void main(String[] args) {
+        String str1 = "ABCDCBA";
+        String str2 = "DCABDC";
+
+        int[][] arr = new int[str2.length()][str1.length()];
+    }
+}
