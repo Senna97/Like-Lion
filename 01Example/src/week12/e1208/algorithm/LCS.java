834fe62a86ae6db613b2e7a29a5531479da030f7
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-12-09 09:23:06 +0900

    실습: loop 돌리기

--- 01Example/src/week12/e1208/algorithm/LCS.java
+++ 01Example/src/week12/e1208/algorithm/LCS.java
@@ -8,5 +8,11 @@ public class LCS {
         String str2 = "DCABDC";
 
         int[][] arr = new int[str2.length()][str1.length()];
+
+        for (int i = 0; i < str2.length(); i++) {
+            for (int j = 0; j < str1.length(); j++) {
+                System.out.printf("str2: %s, str1: %s\n", str2.charAt(i), str1.charAt(j));
+            }
+        }
     }
 }
